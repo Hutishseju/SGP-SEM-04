df640b6cd02b646198501f6a8976ffa7e3557c67
20IT127 <82073450+Hutishseju@users.noreply.github.com>
2022-04-16 21:27:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>login page using css</title>
+    <link rel="stylesheet" href="loginstyle.css">
+</head>
+
+<body>
+    <div class="login-frame">
+        <h2>Welcome to WRITE BOARD</h2>
+        <form action="" method="post">
+            <div class="user-frame">
+                <input type="text" name="" required="">
+                <label>Name:</label>
+            </div>
+            <div class="user-frame">
+                <input type="email" name="" required="">
+                <label>Email:</label>
+            </div>
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span><a href="./todolist.html"> Submit </a>
+            </a>
+    </div>
+    </form>
+</body>
+
+</html>
